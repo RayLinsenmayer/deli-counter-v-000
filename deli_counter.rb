431d71ca2dbf233934f6d91553d9018f7b2c834a
Ray Linsenmayer <raymond_linsenmayer@hotmail.com>
2016-12-08 12:45:48 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,17 @@
-# Write your code here.
+def line(people)
+  if people == []
+      puts "The line is currently empty."
+    else
+    katz_deli =[]
+    people.each_with_index do |person, index|
+      katz_deli.push("The line is currently #{index}. #{person}")
+    end
+
+    return katz_deli
+
+  end
+end
+
+def take_a_number(katz_deli, name)
+  
+end
